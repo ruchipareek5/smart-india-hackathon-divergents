a14241058b7e7b9f47d4002105e12e5f7e1d74b5
ruchipareek5 <ruchipareek5@gmail.com>
2018-03-20 16:33:01 +0530

    Author                      :Ruchi Pareek Description                 :create schema and user in database

--- DBScript/DbScript.sql
+++ DBScript/DbScript.sql
@@ -1,6 +1,4 @@
-{
-	"userEmail" : "ruchi@gmail.com",
-	"userPassword" : "ruchi",
-	"userName" : "ruchipareek",
-	"userMobileNumber" : "9898989898"
-}
+CREATE SCHEMA IF NOT EXISTS msde;
+GRANT ALL PRIVILEGES ON *.* TO 'msde_db_user'@'localhost' IDENTIFIED BY 'Msde1234$$' with GRANT option;
+GRANT SELECT  ON `msde`.* TO 'msde_user'@'localhost' IDENTIFIED BY 'myteam';
+GRANT SELECT,UPDATE,INSERT,DELETE  ON `msde`.* TO 'msde_app_user'@'localhost' IDENTIFIED BY 'MsdeApp123$$';
